ec387ab412053c88cb7546b2becd27fadd1d92ed
Aria Stewart <aredridel@dinhe.net>
2018-11-05 13:32:31 -0500

    ESM only

--- rollup.config.js
+++ rollup.config.js
@@ -1,31 +1,6 @@
-import resolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
-import babel from 'rollup-plugin-babel'
-import uglify from 'rollup-plugin-uglify'
-import filesize from 'rollup-plugin-filesize'
 import pkg from './package.json'
 
 export default [
-  // browser-friendly UMD build
-  {
-    input: 'src/index.js',
-    output: {
-      file: pkg.browser,
-      format: 'umd'
-    },
-    name: 'scroll-snap-polyfill',
-    plugins: [
-      resolve(), // so Rollup can find `ms`
-      commonjs(), // so Rollup can convert `ms` to an ES module
-      babel({
-        exclude: 'node_modules/**'
-      }),
-      uglify(),
-      filesize()
-    ],
-    sourcemap: true
-  },
-
   // CommonJS (for Node) and ES module (for bundlers) build.
   // (We could have three entries in the configuration array
   // instead of two, but it's quicker to generate multiple
@@ -36,8 +11,7 @@ export default [
     input: 'src/index.js',
     external: [],
     output: [
-      { file: pkg.main, format: 'cjs' },
-      { file: pkg.module, format: 'es' }
+      { file: pkg.main, format: 'es' }
     ]
   }
 ]
